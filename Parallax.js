caa00a90d958ae879dd268fd1290017b17329668
amirhachani <hachaniami@gmail.com>
2021-03-16 13:40:26 +0100

    init

---
+++ Parallax.js
@@ -0,0 +1,17 @@
+function checkBudget(budget){
+    if(budget<1000){
+        
+    }
+    if(budget>=1000 && budget<=2000){
+        
+    }
+    if(budget>2000){
+        
+    }
+}
+
+$(document).ready(function(){
+    $('h3').fadeOut(2500).fadeIn(3000)
+})
+
+
